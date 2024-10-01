41ccfe722fe82a6c45b2f55c1c3ef266b9c99558
asecHTL <HumerS22022@sus.htl-grieskirchen.at>
2024-10-01 15:44:39 +0200

    Latest Changes --> Task 3 in progress

--- StreamJavaProject/src/Main.java
+++ StreamJavaProject/src/Main.java
@@ -3,6 +3,7 @@ import com.sun.xml.internal.messaging.saaj.util.FinalArrayList;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,12 +29,29 @@ public class Main {
                 ))
                 .collect(Collectors.toList());
 
-        System.out.println("Ausgabe von Liste (unsotiert) + \n");
-        list.forEach(System.out::println);
 
-        System.out.println("Ausgabe von Liste (sotiert) + \n");
-        list.sort((o1, o2) -> Integer.compare(o2.getDamage(), o1.getDamage()));
-        list.forEach(System.out::println);
+
+        System.out.println("Task 2  \n");
+        System.out.println("Ausgabe von Liste (sotiert)  \n");
+        List<Weapon>listTask2 = list;
+        listTask2.sort((o1, o2) -> Integer.compare(o2.getDamage(), o1.getDamage()));
+        listTask2.forEach(System.out::println);
+
+        System.out.println("Task 3");
+        System.out.println("Ausgabe von Liste (sotiert)  \n");
+        List<Weapon>listTask3 = list;
+        listTask3.sort((o1, o2) -> o1.getCombatType().compareTo(o2.getCombatType()));
+        listTask3.sort((o1, o2) -> o1.getDamageType().compareTo(o2.getDamageType()));
+        listTask3.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+        listTask3.sort(Comparator
+                .comparing((o1, o2) -> o1.getCombatType().compareTo(o2.getCombatType()));
+                //.comparing((o1, o2) -> o1.getCombatType().compareTo(o2.getCombatType())));
+
+        listTask3.forEach(System.out::println);
+
+        System.out.println("Task 4");
+
+
     }
 
 }
